650f934f62ef636ddda557fe9b195bbced741e49
Your Name <you@example.com>
2021-01-04 23:15:07 +0100

    Add 1.4 link do strony 'przepisy'

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.shortcuts import render
 from django.views import View
 
@@ -7,5 +5,10 @@ from django.views import View
 class IndexView(View):
 
     def get(self, request):
-        ctx = {"actual_date": datetime.now()}
-        return render(request, "test.html", ctx)
+        return render(request, "index.html")
+
+
+class RecipeListView(View):
+
+    def get(self, request):
+        return render(request, "app-recipes.html")
