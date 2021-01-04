0902ba69983f31d93aa6ed421281a06c7f2bb27d
Your Name <you@example.com>
2021-01-04 23:44:08 +0100

    Add 2.1 dashboard

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -9,3 +9,9 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+
+class MainView(View):
+
+    def get(self, request):
+        return render(request, "dashboard.html")
