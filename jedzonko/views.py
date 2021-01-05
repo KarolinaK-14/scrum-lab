fba9635d4450b0bd5e9e00458e63dd90b71ba2d8
Synnysyn <69205616+Synnysyn@users.noreply.github.com>
2021-01-05 17:14:35 +0100

    teraz prawidłowo wykrywa oba opisy

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -27,13 +27,16 @@ class AddRecipe(View):
         ingredients = request.POST.get('ingredients')
         description = request.POST.get('description')
         preparation_time = request.POST.get('preparation_time')
+        description_add = request.POST.get('description_add')
 
-        if name !="" and ingredients !="" and description !="" and preparation_time !="":
+        if name !="" and ingredients !="" and description !="" and preparation_time !="" and description_add !="":
+            description += f"\n{description_add}"
+
             recipe = Recipe(name=name, ingredients=ingredients, description=description, preparation_time=preparation_time)
             recipe.save()
             
-            # example = f"{new_recipe.name}, {new_recipe.ingredients}, {new_recipe.description}, {new_recipe.preparation_time}"
-            # return HttpResponse(example)
+            example = f"{recipe.name}, {recipe.ingredients}, {recipe.description}, {recipe.preparation_time}"
+            return HttpResponse(example)
 
             return render(request, 'app-recipes.html')
         
