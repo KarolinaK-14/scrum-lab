93662cf25d39b31409daa439b2c8cee6bed2e664
Your Name <you@example.com>
2021-01-05 14:10:45 +0100

    Add 2.2 menu boczne i urls

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,11 +1,57 @@
 from datetime import datetime
 
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 
 class IndexView(View):
-
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
-        return render(request, "test.html", ctx)
+        return render(request, "index.html", ctx)
+
+
+class MainView(View):
+    def get(self, request):
+        return render(request, "dashboard.html")
+
+
+class RecipeListView(View):
+    def get(self, request):
+        return HttpResponse()
+
+
+class RecipeView(View):
+    def get(self, request):
+        return HttpResponse()
+
+
+class AddRecipeView(View):
+    def get(self, request):
+        return HttpResponse()
+
+
+class ModifyRecipeView(View):
+    def get(self, request):
+        return HttpResponse()
+
+
+class PlanListView(View):
+    def get(self, request):
+        return HttpResponse()
+
+
+class PlanView(View):
+    def get(self, request):
+        return HttpResponse()
+
+
+class AddPlanView(View):
+    def get(self, request):
+        return HttpResponse()
+
+
+class PlanAddRecipeView(View):
+    def get(self, request):
+        return HttpResponse()
+
--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from jedzonko.views import IndexView
+from jedzonko import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', IndexView.as_view()),
+    path('', v.IndexView.as_view(), name='index'),
+    path('main/', v.MainView.as_view(), name='main'),
+    path('recipe/<int:id>/', v.RecipeView.as_view(), name='recipe'),
+    path('recipe/list/', v.RecipeListView.as_view(), name='recipe-list'),
+    path('recipe/add/', v.AddRecipeView.as_view(), name='add-recipe'),
+    path('recipe/modify/<int:id>/', v.ModifyRecipeView.as_view(), name='modify-recipe'),
+    path('plan/list/', v.PlanListView.as_view(), name='plan-list'),
+    path('plan/<int:id>/', v.PlanView.as_view(), name='plan'),
+    path('plan/add/', v.AddPlanView.as_view(), name='add-plan'),
+    path('plan/add-recipe/', v.PlanAddRecipeView.as_view(), name='plan-add-recipe')
+
 ]
