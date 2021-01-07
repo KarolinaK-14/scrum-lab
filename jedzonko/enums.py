d04f3f6180a0e93b70647b9399ce32ad147b7976
Your Name <you@example.com>
2021-01-07 05:09:41 +0100

    Add 5.1 aktualizacja modeli

---
+++ jedzonko/enums.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class DayName(Enum):
+    MONDAY = "poniedziałek"
+    TUESDAY = "wtorek"
+    WEDNESDAY = "środa"
+    THURSDAY = "czwartek"
+    FRIDAY = "piątek"
+    SATURDAY = "sobota"
+    SUNDAY = "niedziela"
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-from datetime import datetime
+from .enums import DayName
 
-# Create your models here.
 
 class Recipe(models.Model):
     """
-      - name: nazwa przepisu,
-  - ingredients: składniki przepisu,
-  - description: treść przepisu,
-  - created: data dodania przepisu (wypełniane automatycznie),
-  - updated: data aktualizacji przepisu (wypełniane automatycznie),
-  - preparation_time: czas przygotowania (w minutach),
-  - votes: liczba głosów na przepis (domyślnie 0)
+    - name: nazwa przepisu,
+    - ingredients: składniki przepisu,
+    - description: treść przepisu,
+    - created: data dodania przepisu (wypełniane automatycznie),
+    - updated: data aktualizacji przepisu (wypełniane automatycznie),
+    - preparation_time: czas przygotowania (w minutach),
+    - votes: liczba głosów na przepis (domyślnie 0)
     """
     name = models.CharField(max_length=255)
     ingredients = models.TextField()
@@ -20,3 +19,31 @@ class Recipe(models.Model):
     updated = models.DateTimeField(auto_now=True)
     preparation_time = models.IntegerField()
     votes = models.IntegerField(default=0)
+
+
+class Plan (models.Model):
+    """
+    - name: nazwa planu,
+    - description: opis planu,
+    - created: data utworzenia.
+    - recipes - relacja wiele do wielu do modelu przepisu. Wykorzystaj model pośredni recipeplan (through)
+    """
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    recipes = models.ManyToManyField(Recipe, through="RecipePlan")
+
+
+class RecipePlan(models.Model):
+    """
+    - meal_name: nazwa posiłku (śniadanie, obiad itp),
+    - recipe: relacja do tabeli przepisów
+    - plan: relacja do tabeli planów
+    - order: kolejność posiłków w planie,
+    - day_name: "from .enums import DayName"
+    """
+    meal_name = models.CharField(max_length=255)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    day_name = models.CharField(max_length=16, choices=DayName.choices())
