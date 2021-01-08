cb9e072f1fbecec3391d79b3aaaf793e9fc60866
Synnysyn <69205616+Synnysyn@users.noreply.github.com>
2021-01-08 10:40:25 +0100

    system pulubień

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -133,9 +133,15 @@ class RecipeView(View):
             'preparation': recipe.preparation,
             'preparation_time': recipe.preparation_time,
             'votes': recipe.votes,
+            'id' : recipe.id,
         }
         return render(request, "app-recipe-details.html", context)
-      
+    
+    def post (self, request, recipe_id):
+        recipe = Recipe.objects.get(id=recipe_id)
+        recipe.votes += 1
+        recipe.save()
+        return redirect('recipe', recipe_id)
 
 class PlanAddRecipeView(View):
     def get(self, request):
